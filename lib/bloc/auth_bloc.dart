e8aef8efd7c28c0a7c0ec78f04410380d6b2837a
Lais Alexandre <lais.an@yahoo.com.br>
2024-08-13 16:04:05 -0300

    final adjustments to home screen and events

--- lib/bloc/auth_bloc.dart
+++ lib/bloc/auth_bloc.dart
@@ -6,26 +6,46 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
-    on<AuthLoginRequested>((event, emit) async {
-      try {
-        final email = event.email;
-        final password = event.password;
-        final emailRegex = RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z]+\.[a-zA-Z]+');
+    on<AuthLoginRequested>(_onAuthLoginRequested);
 
-        if (!emailRegex.hasMatch(email)) {
-          return emit(AuthFailure('Invalid email'));
-        }
+    on<AuthLogoutRequested>(_onAuthLogoutRequested);
+  }
 
-        if (password.length < 6) {
-          return emit(AuthFailure('Password cannot be less than 6 characters'));
-        }
+  void _onAuthLoginRequested(
+    AuthLoginRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    try {
+      final email = event.email;
+      final password = event.password;
+      final emailRegex = RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z]+\.[a-zA-Z]+');
 
-        await Future.delayed(const Duration(seconds: 1), () {
-          return emit(AuthSuccess(uid: '$email-$password'));
-        });
-      } catch (e) {
-        return emit(AuthFailure(e.toString()));
+      if (!emailRegex.hasMatch(email)) {
+        return emit(AuthFailure('Invalid email'));
       }
-    });
+
+      if (password.length < 6) {
+        return emit(AuthFailure('Password cannot be less than 6 characters'));
+      }
+
+      await Future.delayed(const Duration(seconds: 1), () {
+        return emit(AuthSuccess(uid: '$email-$password'));
+      });
+    } catch (e) {
+      return emit(AuthFailure(e.toString()));
+    }
+  }
+
+  void _onAuthLogoutRequested(
+    AuthLogoutRequested event,
+    Emitter<AuthState> emit,
+  ) {
+    emit(AuthLoading());
+    try {
+      return emit(AuthInitial());
+    } catch (e) {
+      emit(AuthFailure(e.toString()));
+    }
   }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login_form/bloc/auth_bloc.dart';
+import 'package:login_form/login_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,6 +10,47 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Column(
+        children: [
+          BlocConsumer<AuthBloc, AuthState>(
+            listener: (context, state) {
+              if (state is AuthInitial) {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LoginScreen()),
+                    (route) => false);
+              }
+            },
+            builder: (context, state) {
+              if (state is AuthLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              return Center(
+                child: Text((state as AuthSuccess).uid),
+              );
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              context.read<AuthBloc>().add(AuthLogoutRequested());
+            },
+            child: const Text(
+              'Logout',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 17,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
